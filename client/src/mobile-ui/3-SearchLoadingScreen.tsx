6fdd5333bd295bd4507c281c9f24c057ec4a967e
Marcus Westin <marcus.westin@gmail.com>
2022-12-15 15:14:02 -0500

    Implement search loading screen stub

--- client/src/mobile-ui/3-SearchLoadingScreen.tsx
+++ client/src/mobile-ui/3-SearchLoadingScreen.tsx
@@ -1,3 +1,25 @@
+import { useEffect } from 'react'
+import store from './data/store'
+import { Button, Col } from './ui-lib'
+
 export function SearchLoadingScreen() {
-  return <div>loading...</div>
+  useEffect(() => {
+    let timeout = setTimeout(() => {
+      store.screen.navigateTo('SearchResultScreen')
+    }, 1000)
+    return () => clearTimeout(timeout)
+  })
+
+  return (
+    <Col
+      style={{ justifyContent: 'center', textAlign: 'center' }}
+      onClick={() => {
+        store.screen.navigateTo('SearchResultScreen')
+      }}>
+      Searching 100s of flights...
+      <Button onClick={() => store.screen.navigateTo('SearchResultScreen')}>
+        Continue
+      </Button>
+    </Col>
+  )
 }
---
+++ client/src/mobile-ui/4-SearchResultsScreen.tsx
@@ -0,0 +1,3 @@
+export function SearchResultsScreen() {
+  return <div>SearchResultsScreen</div>
+}
