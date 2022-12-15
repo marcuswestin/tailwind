21179f43a24bfab41de0531bbb11584150a798d3
Marcus Westin <marcus.westin@gmail.com>
2022-12-15 15:13:45 -0500

    Comment out duffel UI

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
-import AppDuffelCreated from './default-duffel-app/AppDuffelCreated'
+// import AppDuffelCreated from './default-duffel-app/AppDuffelCreated'
 import { MobileUI } from './mobile-ui/MobileUI'
 
 export default function App() {
   return <MobileUI />
-  return <AppDuffelCreated />
+  // return <AppDuffelCreated />
 }
