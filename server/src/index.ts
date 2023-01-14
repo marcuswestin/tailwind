f8ba407956d366c46564a5550e829e36f4b3c83c
Marcus Westin <marcus.westin@gmail.com>
2023-01-14 11:18:15 -0500

    Start serving app on ports 4000 and 4001

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,7 +4,7 @@ import './env-setup'
 import app from './server'
 
 // Start the server
-const port = Number(process.env.PORT || 3001)
+const port = Number(process.env.PORT || 4001)
 app.listen(port, () => {
   console.info('Express server started on port: ' + port)
 })
